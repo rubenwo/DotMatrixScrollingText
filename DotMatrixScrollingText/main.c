a5b019d41d76398c7bc00a860072b98e5a083b4f
Ruben <rwoldhui@avans.nl>
2019-04-05 16:56:19 +0200

    [Initial] project push

---
+++ DotMatrixScrollingText/main.c
@@ -0,0 +1,18 @@
+/*
+ * DotMatrixScrollingText.c
+ *
+ * Created: 4/5/2019 4:54:37 PM
+ * Author : diabl
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
